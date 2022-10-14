3195110459d3eff458f1385cd1c4b945932659be
Vittorioemot <105027649+Vittorioemot@users.noreply.github.com>
2022-10-14 17:03:37 +0800

    pages updates and index js

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -13,7 +13,7 @@ const Header = () => {
       <div className="container flex-row justify-space-between-lg justify-center align-center">
         <div>
           <Link className="text-light" to="/">
-            <h1 className="m-0">Wedding Photography Matching Service</h1>
+            <h1 className="m-0">Wedding Photography Forum</h1>
           </Link>
           <p className="m-0">Find the Perfect Wedding Photographer.</p>
         </div>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -19,6 +19,20 @@ const Home = () => {
         >
           <ThoughtForm />
         </div>
+        <div className="card col-12 col-md-8 mb-3">
+          <h2>
+            Useful Links
+          </h2>
+          <a href="https://emot.co/featured-weddings/">View Gallery</a>
+          <br></br>
+          <br></br>
+          <a href="https://emot.co/check-availability/">Check Availability</a>
+          <br></br>
+          <br></br>
+          <a href="https://emot.co/booking/">Book Now</a>
+          <br></br>
+          <br></br>
+        </div>
         <div className="col-12 col-md-8 mb-3">
           {loading ? (
             <div>Loading...</div>
